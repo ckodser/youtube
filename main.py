a0a83ed4d87810bbdee2a6f539b9b05badba0eb1
ckodser <arshia.soltani2@gmail.com>
2022-08-15 14:45:37 +0430

    prevent server error

--- main.py
+++ main.py
@@ -97,105 +97,109 @@ def is_match(url, split_url):
 
 
 def start_listening(HOST, PORT, function_url_list, admin, allowed_ip):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((HOST, PORT))
-        while True:
-            s.listen()
-            conn = None
-            try:
-                conn, addr = s.accept()
-                if allowed_ip is None:
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                s.bind((HOST, PORT))
+                while True:
+                    s.listen()
+                    conn = None
                     try:
-                        Database().check_ip(addr[0])
-                    except Exception as e:
-                        print("Oh no! {}".format(e))
-                        conn.close()
-                        continue
-                if allowed_ip is not None and allowed_ip != addr[0]:
-                    text = f"USE PROXY! you should use IP:{allowed_ip}  but you use IP:{addr[0]}"
-                    print(text)
-                    answer = error_page({"url": text}, function_url_list)
-                    answer = answer.encode()
-                    conn.sendall(answer)
-                    conn.close()
-                    continue
-                with conn:
-                    data = conn.recv(50000000)
-                    form_parts = []
-                    if not "multipart/form-data".encode() in data:
-                        coded_data = copy.deepcopy(data)
-                        data = data.decode()
-                    else:
-                        content_length_start = data.find("Content-Length".encode()) + 16
-                        content_length_length = data[content_length_start:].find("\r\n".encode())
-                        content_length = data[
-                                         content_length_start:content_length_start + content_length_length].decode()
-                        content_length = int(content_length)
-                        if content_length > 50000000:
-                            print("LARGE FILE!")
-                            answer = error_page(request_dict, function_url_list)
+                        conn, addr = s.accept()
+                        if allowed_ip is None:
+                            try:
+                                Database().check_ip(addr[0])
+                            except Exception as e:
+                                print("Oh no! {}".format(e))
+                                conn.close()
+                                continue
+                        if allowed_ip is not None and allowed_ip != addr[0]:
+                            text = f"USE PROXY! you should use IP:{allowed_ip}  but you use IP:{addr[0]}"
+                            print(text)
+                            answer = error_page({"url": text}, function_url_list)
                             answer = answer.encode()
                             conn.sendall(answer)
                             conn.close()
                             continue
-                        while len(data) < content_length:
-                            data += conn.recv(50000000)
-                        coded_data = copy.deepcopy(data)
-                        header_end = data.find("\r\n\r\n".encode())
-                        header = data[:header_end].decode()
-                        rest = data[header_end + 4:]
-
-                        while len(rest) > 0:
-                            header_end = rest.find("\r\n\r\n".encode())
-                            form_header = rest[:header_end].decode().split("\n")[1:]
-                            rest = rest[header_end + 4:]
-                            if rest.find("\r\nContent-Disposition: form-data; name=".encode()) != -1:
-                                end_data = rest.find("\r\nContent-Disposition: form-data; name=".encode())
-                                form_data = rest[:end_data]
-                                rest = rest[end_data + 2:]
+                        with conn:
+                            data = conn.recv(50000000)
+                            form_parts = []
+                            if not "multipart/form-data".encode() in data:
+                                coded_data = copy.deepcopy(data)
+                                data = data.decode()
                             else:
-                                break
-                            form_parts.append((form_header, form_data))
-                        data = header
+                                content_length_start = data.find("Content-Length".encode()) + 16
+                                content_length_length = data[content_length_start:].find("\r\n".encode())
+                                content_length = data[
+                                                 content_length_start:content_length_start + content_length_length].decode()
+                                content_length = int(content_length)
+                                if content_length > 50000000:
+                                    print("LARGE FILE!")
+                                    answer = error_page(request_dict, function_url_list)
+                                    answer = answer.encode()
+                                    conn.sendall(answer)
+                                    conn.close()
+                                    continue
+                                while len(data) < content_length:
+                                    data += conn.recv(50000000)
+                                coded_data = copy.deepcopy(data)
+                                header_end = data.find("\r\n\r\n".encode())
+                                header = data[:header_end].decode()
+                                rest = data[header_end + 4:]
 
-                    if len(data) == 0:
-                        continue
-                    split_data = data.split()
-                    request_dict = to_request_dict(data.split("\n")[1:])
-                    method = split_data[0]
-                    url = split_data[1]
-                    request_dict["method"] = method
-                    request_dict["url"] = url
-                    request_dict["body"] = data[data.find("\r\n\r\n") + 4:]
-                    request_dict["form_parts"] = form_parts
-                    request_dict["admin"] = admin
-                    request_dict["packet"] = coded_data
-                    request_dict["conn"] = conn
-                    split_url = url.lstrip("/").split("/")
-                    answer = 404
-                    for function, url in function_url_list:
-                        if is_match(url, split_url):
-                            parameters = get_parameters(url, split_url)
-                            parameters["request_dict"] = request_dict
-                            answer = function(**parameters)
-                            break
-                    if answer is None:
-                        conn.close()
-                        continue
-                    if answer == 404:
-                        print(f"Connected by {addr}")
-                        print(method, url, request_dict)
-                        print("ERROR")
-                        answer = error_page(request_dict, function_url_list)
-                    if answer.__class__ == str:
-                        answer = answer.encode()
-                    conn.sendall(answer)
-            except KeyboardInterrupt:
-                print("conn closed...")
-                if conn is not None:
-                    conn.close()
-                break
+                                while len(rest) > 0:
+                                    header_end = rest.find("\r\n\r\n".encode())
+                                    form_header = rest[:header_end].decode().split("\n")[1:]
+                                    rest = rest[header_end + 4:]
+                                    if rest.find("\r\nContent-Disposition: form-data; name=".encode()) != -1:
+                                        end_data = rest.find("\r\nContent-Disposition: form-data; name=".encode())
+                                        form_data = rest[:end_data]
+                                        rest = rest[end_data + 2:]
+                                    else:
+                                        break
+                                    form_parts.append((form_header, form_data))
+                                data = header
+
+                            if len(data) == 0:
+                                continue
+                            split_data = data.split()
+                            request_dict = to_request_dict(data.split("\n")[1:])
+                            method = split_data[0]
+                            url = split_data[1]
+                            request_dict["method"] = method
+                            request_dict["url"] = url
+                            request_dict["body"] = data[data.find("\r\n\r\n") + 4:]
+                            request_dict["form_parts"] = form_parts
+                            request_dict["admin"] = admin
+                            request_dict["packet"] = coded_data
+                            request_dict["conn"] = conn
+                            split_url = url.lstrip("/").split("/")
+                            answer = 404
+                            for function, url in function_url_list:
+                                if is_match(url, split_url):
+                                    parameters = get_parameters(url, split_url)
+                                    parameters["request_dict"] = request_dict
+                                    answer = function(**parameters)
+                                    break
+                            if answer is None:
+                                conn.close()
+                                continue
+                            if answer == 404:
+                                print(f"Connected by {addr}")
+                                print(method, url, request_dict)
+                                print("ERROR")
+                                answer = error_page(request_dict, function_url_list)
+                            if answer.__class__ == str:
+                                answer = answer.encode()
+                            conn.sendall(answer)
+                    except KeyboardInterrupt:
+                        print("conn closed...")
+                        if conn is not None:
+                            conn.close()
+        except:
+            print("server error happened!")
+
     print("Server stopped, Good Bye!")
 
 
