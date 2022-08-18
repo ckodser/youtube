5d566902242c9434aa298bd90e56b6c257e1d972
ckodser <arshia.soltani2@gmail.com>
2022-08-18 15:10:34 +0430

    debug error page

--- templates/error/view.py
+++ templates/error/view.py
@@ -2,10 +2,10 @@ from templates.utils import http_ok_header
 
 def error_page(request_dict, function_url_list):
     print("request dict", len(request_dict), request_dict)
-    list = ""
+    header_list = ""
     for k in request_dict.keys():
         if k != "Cookie":
-            list += f"<li> {k}={request_dict[k]}</li>\n"
+            header_list += f"<li> {k}={request_dict[k]}</li>\n"
     cookie_list = ""
     if "Cookie" in request_dict:
         for cookie_name in request_dict["Cookie"].keys():
@@ -20,4 +20,4 @@ def error_page(request_dict, function_url_list):
         url = url.replace(">", "*")
         list += f"<li> {url} </li>\n"
     with open("templates/error/index.html") as index:
-        return http_ok_header([("bardia1", 4), ("bardia2","rtrt")])+index.read().replace("{{LIST}}", list).replace("{{URL}}", request_dict['url']).replace("{{list}}", list).replace("{{cookie_list}}", cookie_list)
+        return http_ok_header([("bardia1", 4), ("bardia2","rtrt")])+index.read().replace("{{LIST}}", list).replace("{{URL}}", request_dict['url']).replace("{{header_list}}", header_list).replace("{{cookie_list}}", cookie_list)
